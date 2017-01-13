89e2c937220e73d5cbf98a899b6378395bbbc1f6
douglas <964273641@126.com>
2017-01-13 18:29:47 +0800

    add

---
+++ src/components/Piclist.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class = "picList">
+        <div>
+            <ul class="imgList">
+                <li v-for = 'info in infos'>
+                    <img :src = "info.src" />
+                </li>
+            </ul>
+        </div>
+    </div>    
+</template>
+
+<style>
+.picList {
+    height: auto;
+    width: 100%;  
+}
+
+.imgList {
+    padding: 10px 5px;
+}
+
+.imgList li {
+    padding-bottom: 7px;
+}
+
+.imgList img {
+    width: 100%;
+    height: 120px;
+    border-radius: 5px;
+}
+</style>
+
+<script>
+  export default {
+      name: 'my-plist',
+
+      data () {
+          return {
+            infos: [
+                        {
+                            title:'测试1',
+                            author:'作者1',
+                            src : 'src/assets/1.jpg',
+                            scanCount: 123,
+                            commentCount: 65,
+                            praiseCount: 22
+                        },
+                        {
+                            title:'测试2',
+                            author:'作者2',
+                            src : 'src/assets/2.jpg',
+                            scanCount: 123,
+                            commentCount: 65,
+                            praiseCount: 22
+                        },
+                        {
+                            title:'测试3',
+                            author:'作者1',
+                            src : 'src/assets/3.jpg',
+                            scanCount: 222,
+                            commentCount: 12,
+                            praiseCount: 33
+                        },
+                        {
+                            title:'测试4',
+                            author:'作者4',
+                            src : 'src/assets/4.jpg',
+                            scanCount: 123,
+                            commentCount: 65,
+                            praiseCount: 22
+                        },
+                        {
+                            title:'测试1',
+                            author:'作者1',
+                            src : 'src/assets/5.jpg',
+                            scanCount: 123,
+                            commentCount: 65,
+                            praiseCount: 22
+                        },
+                        {
+                            title:'测试1',
+                            author:'作者1',
+                            src : 'src/assets/6.jpg',
+                            scanCount: 123,
+                            commentCount: 65,
+                            praiseCount: 22
+                        }
+                    ]
+            }
+      }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Icon from 'vue-svg-icon/Icon.vue';
+Vue.component('icon', Icon);  
+Icon.inject('account'); // the name of your svg pic
+Icon.inject('add'); // the name of your svg pic
+Icon.inject('favorite'); // the name of your svg pic
 
 new Vue({
   el: '#app',
-  // components : {
-  //   'app': App
-  // }
+
   render: h => h(App)
 })
