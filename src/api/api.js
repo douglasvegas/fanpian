57cae56bde833f2c3e819cf5145ae87a8aec7264
孙鑫 <sunxin@sunxindeMacBook-Pro.local>
2017-01-15 20:45:48 +0800

    modify style

---
+++ src/api/api.js
@@ -0,0 +1,52 @@
+module.exports = {
+    infos: [
+            {
+                title:'测试1',
+                author:'作者1',
+                src : 'src/assets/1.jpg',
+                scanCount: 123,
+                commentCount: 65,
+                praiseCount: 22
+            },
+            {
+                title:'测试2',
+                author:'作者2',
+                src : 'src/assets/2.jpg',
+                scanCount: 123,
+                commentCount: 65,
+                praiseCount: 22
+            },
+            {
+                title:'测试3',
+                author:'作者1',
+                src : 'src/assets/3.jpg',
+                scanCount: 222,
+                commentCount: 12,
+                praiseCount: 33
+            },
+            {
+                title:'测试4',
+                author:'作者4',
+                src : 'src/assets/4.jpg',
+                scanCount: 123,
+                commentCount: 65,
+                praiseCount: 22
+            },
+            {
+                title:'测试1',
+                author:'作者1',
+                src : 'src/assets/5.jpg',
+                scanCount: 123,
+                commentCount: 65,
+                praiseCount: 22
+            },
+            {
+                title:'测试1',
+                author:'作者1',
+                src : 'src/assets/6.jpg',
+                scanCount: 123,
+                commentCount: 65,
+                praiseCount: 22
+            }
+           ]
+}
