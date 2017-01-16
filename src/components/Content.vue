61fe36047e0f925caf1484ba8918cb76e59468ec
孙鑫 <sunxin@sunxindeMacBook-Pro.local>
2017-01-16 23:53:17 +0800

    add refresh but still has some bugs

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
+    <div @touchstart= 'handleTouchStart' 
+         @touchmove = 'handleTouchMove'
+         @touchend  = 'handleTouchEnd'
+    >
+        
         <MySwipe></MySwipe>
+        <p>{{msg}}</p>
         <MyPlist></MyPlist>
         <!--<router-view></router-view>-->
+
     </div>
 </template>
 <script>
@@ -17,7 +23,36 @@ import MyPlist from './Piclist.vue'
         },
         data () {
             return {
+                msg:'2',
+                start:'',
+                end: ''
+            }
+        },
+        methods : {
+            handleTouchStart: function (e) {
+                var _y=e.changedTouches[0].pageY;
+                this.start = _y;
+            },
+            handleTouchMove: function (e) {
+                var _x=e.changedTouches[0].pageX;
+                var _y=e.changedTouches[0].pageY;
+               
+                this.msg = parseInt(this.end) - parseInt(this.start);
+               
+                if (this.msg > 50) {
+                    this.$emit('changeLoading')
+                }
+            },
+            handleTouchEnd: function (e) {
+                var _x=e.changedTouches[0].pageX;
+                var _y=e.changedTouches[0].pageY;
 
+                this.end = _y;
+                this.msg = parseInt(this.end) - parseInt(this.start);
+               
+                console.log(parseInt(this.end) - parseInt(this.start));
+                this.$emit('resetLoading')
+                // this.msg = _y;
             }
         }
     }
