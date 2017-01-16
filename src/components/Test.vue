085182cd37e1ddc83da1da8d1d13e114041868f9
douglas <964273641@126.com>
2017-01-16 17:51:36 +0800

    add vue-router

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>{{$route.params.id}}</p>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'this is a test'
+            }
+        }
+    }
+</script>
