4237921244c499c09d02f4b09b8a6a0c460051ac
孙鑫 <sunxin@sunxindeMacBook-Pro.local>
2017-01-17 22:06:04 +0800

    waste

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routers.js'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 
 let router = new VueRouter({
